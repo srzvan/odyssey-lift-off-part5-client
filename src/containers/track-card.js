3894173a66655a5d27624fa5bdbd0b77a2ba6633
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-01-19 18:00:12 +0200

    replace deprecated length with durationInSeconds

--- src/containers/track-card.js
+++ src/containers/track-card.js
@@ -28,14 +28,10 @@ export const INCREMENT_TRACK_VIEWS = gql`
  * for each track populating the tracks grid homepage.
  */
 const TrackCard = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount, id } = track;
+  const { title, thumbnail, author, durationInSeconds, modulesCount, id } = track;
 
   const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
     variables: { incrementTrackViewsId: id },
-    // to observe what the mutation response returns
-    onCompleted: (data) => {
-      console.log(data);
-    },
   });
 
   return (
@@ -51,7 +47,7 @@ const TrackCard = ({ track }) => {
             <AuthorAndTrack>
               <AuthorName>{author.name}</AuthorName>
               <TrackLength>
-                {modulesCount} modules - {humanReadableTimeFromSeconds(length)}
+                {modulesCount} modules - {humanReadableTimeFromSeconds(durationInSeconds)}
               </TrackLength>
             </AuthorAndTrack>
           </CardFooter>
